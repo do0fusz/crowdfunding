8b1cd6f6a034200efa927d10de4e07538fdd0bc4
do0fusz <do0fusz@hotmail.com>
2015-10-04 17:34:00 +0200

    added projects crud

--- Gemfile
+++ Gemfile
@@ -8,14 +8,17 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sass-rails', '~> 5.0.4' 
+
 # my gems 
 gem 'bootstrap-sass', '~> 3.3.5'
 gem 'font-awesome-sass-rails', '~> 3.0.2.2'
 gem 'devise'
+gem 'annotate'
+gem 'simple_form'
+gem 'bootstrap-wysihtml5-rails'
+
 
 gem 'sdoc', '~> 0.4.0', group: :doc
-
-
 
 group :development, :test do
   gem 'byebug'
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,67 @@
 class ProjectsController < ApplicationController
-	
+	before_action :authenticate_user!, except: [:index, :show]
+	before_action :set_project, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@project = Project.all
+		@displayed_projects = Project.take(4)
 	end
+
+	def show 
+	end
+
+
+	def new 
+		@project = Project.new 
+	end
+
+	def create 
+		@project = current_user.projects.build(project_params)
+			respond_to do |format| 
+				if @project.save 
+					format.html{ redirect_to @project, notice: "Project was created"}
+					format.json{ render :show, status: :ok, location: @project}
+				else 
+					format.html{ render 'new', notice: "Fill in the missing fields"}
+					format.json{ render json: @project.errors, status: :unprocessable_entity }
+				end
+		end
+	end
+
+
+	def update 
+		respond_to do |format| 
+			if @project.update(project_params)
+				format.html{ redirect_to @project, notice: "Project was created"}
+				format.json{ render :show, status: :ok, location: @project}
+			else 
+				format.html{ render 'new', notice: "Fill in the missing fields"}
+				format.json{ render json: @project.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	def edit 
+	end
+
+	def destroy 
+		@project.destroy 
+		respond_to do |format | 
+			format.html {redirect_to projects_path, notice: "Project was destroyed"}
+			format.json { head :no_content }
+		end
+	end
+
+
+	private 
+
+		def project_params
+			params.require(:project).permit(:name, :short_description, :description, :image_url, :status, :goal, :expiration_date)
+		end
+
+		def set_project
+			@project = Project.find(params[:id])
+		end
+
 
 end
